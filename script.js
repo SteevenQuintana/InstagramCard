2725f70fbb3602ce3606a6fa6c75dd81fc726aef
SteevenQuintana <stiven0721@hotmail.com>
2022-08-07 22:25:22 -0500

    adding animations

--- script.js
+++ script.js
@@ -1,47 +1,33 @@
+let heart = document.querySelector(".dislike");
+heart.addEventListener("click", () => {
+  heart.classList.toggle("like");
+});
+
+const form = document.querySelector(".add-comment");
+const newComment = document.querySelector(".new-comment");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const comment = document.querySelector(".text").value;
+  if (comment) {
+    const html = `
+    <div class="first-comment">
+      <strong> Comment </strong>
+      <span> ${comment}</span>
+    </div>`;
+    newComment.insertAdjacentHTML("beforebegin", html);
+    document.querySelector(".text").value = "";
+  }
+});
+
 const swiper = new Swiper(".swiper", {
   loop: true,
-
   pagination: {
     el: ".swiper-pagination",
     type: "bullets",
     clickable: true,
   },
-
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
 });
-
-function likeBtn() {
-  let heart = document.querySelector(".dislike");
-  let isPressed = 0;
-  heart.addEventListener("click", () => {
-    console.log("first");
-    isPressed = 1;
-    if (isPressed) {
-      heart.classList.add("like");
-    } else {
-      heart.classList.remove("like");
-      isPressed = 0;
-    }
-  });
-}
-
-const addComment = () => {
-  const form = document.querySelector(".add-comment");
-  const post = document.querySelector(".add-comment");
-  post.innerHTML = "";
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const comment = document.querySelector(".text").value;
-    const html = `
-    <div class="post">
-        <strong> Comment </strong>
-        <span>
-          ${comment}</span
-        >
-      </div>`;
-    post.insertAdjacentHTML("beforebegin", html);
-  });
-};
